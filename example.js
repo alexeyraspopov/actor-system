46aa4340e713010197c875c67177e24ec1e39814
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-03-20 09:01:42 +0200

    better naming for example and index files

---
+++ example.js
@@ -0,0 +1,71 @@
+import MessageDispatcher from './modules/MessageDispatcher';
+import ActorSystem from './modules/ActorSystem';
+
+async function* CounterAct(dispatcher) {
+  for await (const message of dispatcher) {
+    console.log('CounterAct::cycle', message.subject.name);
+    switch (message.subject) {
+    case IncrementCommand:
+      dispatcher.dispatch(new IncrementEvent());
+      break;
+    default:
+    }
+  }
+}
+
+async function* CounterStore(dispatcher) {
+  let state = 0;
+  for await (const message of dispatcher) {
+    const newState = reduce(state, message);
+    if (newState !== state) {
+      state = newState;
+      console.log('CounterStore::yield', message.subject.name, state);
+      dispatcher.dispatch(new StateMessage(state));
+    }
+  }
+}
+
+function reduce(state, message) {
+  switch (message.subject) {
+  case IncrementEvent:
+    return state + 1;
+  default:
+    return state;
+  }
+}
+
+async function* Logger(dispatcher) {
+  for await (const message of dispatcher) {
+    switch (message.subject) {
+    case StateMessage:
+      console.log('Logger::cycle', message.subject.name, message.content);
+      break;
+    default:
+      console.log('Logger::cycle', message.subject.name);
+    }
+  }
+}
+
+async function* Main(dispatcher) {
+  dispatcher.dispatch(new IncrementCommand());
+  dispatcher.dispatch(new IncrementCommand());
+}
+
+class Message {
+  constructor(content) {
+    this.subject = this.constructor;
+    this.content = content;
+  }
+}
+
+class IncrementCommand extends Message {}
+class IncrementEvent extends Message {}
+class StateMessage extends Message {}
+
+const dispatcher = new MessageDispatcher();
+const system = new ActorSystem(dispatcher);
+
+system.spawn(CounterAct);
+system.spawn(CounterStore);
+system.spawn(Logger);
+system.spawn(Main);
