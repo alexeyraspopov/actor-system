1b41308fc9c733b76720827d7ab73bd859b83b17
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-03-20 06:21:57 +0200

    remove unused method from ActorSystem

--- ActorSystem.js
+++ ActorSystem.js
@@ -1,17 +1,9 @@
 export default class ActorSystem {
   constructor(dispatcher) {
     this.dispatcher = dispatcher;
-    this.iterators = new Map();
   }
 
-  async spawn(coroutine) {
-    const iterator = coroutine(this.dispatcher);
-    this.iterators.set(coroutine, iterator);
-    for await (const state of iterator) continue;
-  }
-
-  dispose(coroutine) {
-    const iterator = this.iterators.get(coroutine);
-    if (iterator) iterator.return();
+  spawn(coroutine) {
+    coroutine(this.dispatcher).next();
   }
 }
