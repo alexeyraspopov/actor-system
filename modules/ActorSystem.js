4d0bb8c49f6d23215fb8832f225ac87036fa1605
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-03-29 23:18:10 +0300

    create actor factory method

--- modules/ActorSystem.js
+++ modules/ActorSystem.js
@@ -1,6 +1,15 @@
 export default class ActorSystem {
   constructor(dispatcher) {
     this.dispatcher = dispatcher;
+    this.actors = new Map();
+  }
+
+  actorOf(Actor, name = Actor.constructor.name) {
+    if (this.actors.has(name)) {
+      return this.actors.get(name);
+    }
+    const instance = new Actor();
+    return instance;
   }
 
   spawn(coroutine, ...args) {
