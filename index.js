7aada3d01d7cbb31ead491baeff5badabcf71302
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-03-29 18:41:01 +0300

    make Mailbox instance public

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 export { default as MessageDispatcher } from './modules/MessageDispatcher';
+export { default as Mailbox } from './modules/Mailbox';
 export { default as ActorSystem } from './modules/ActorSystem';
