05e92e9b4ed69a880bbaba3eb901f7a3a21b0028
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-04-16 19:48:06 +0300

    pass disposable to actor

--- modules/Actor.js
+++ modules/Actor.js
@@ -1,4 +1,8 @@
 export default class Actor {
+  constructor(disposable) {
+    this.disposable = disposable;
+  }
+
   receive() {
     throw new Error('Actor::receive is an abstract method and should be overriden');
   }
