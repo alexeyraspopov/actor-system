5e40741324b775ff88eab3607c98813f69dd7f57
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-04-30 14:36:04 +0300

    add missing exports to the entry point

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+export { default as ExecutionContext } from './modules/ExecutionContext';
+export { default as AnimationFrameExecutor } from './modules/AnimationFrameExecutor';
 export { default as MessageDispatcher } from './modules/MessageDispatcher';
 export { default as Mailbox } from './modules/Mailbox';
+export { default as Message } from './modules/Message';
 export { default as ActorSystem } from './modules/ActorSystem';
 export { default as Actor } from './modules/Actor';
