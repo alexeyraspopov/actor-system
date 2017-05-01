659bec8643db08a714ce3755d89a135343198f8c
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-05-01 23:39:08 +0300

    create react application example

---
+++ examples/react-application/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const babelOptions = {
+  babelrc: false,
+  presets: [
+    require.resolve('babel-preset-react'),
+    require.resolve('babel-preset-es2017'),
+    require.resolve('babel-preset-stage-3'),
+  ]
+};
+
+module.exports = {
+  module: {
+    rules: [
+      { test: /js$/, exclude: /node_modules/,
+        use: [{ loader: 'babel-loader',
+                options: babelOptions }] }
+    ]
+  },
+  resolve: {
+    alias: {
+      'actor-system': path.resolve(__dirname, '../../'),
+    }
+  },
+  devtool: 'inline-source-map'
+};
