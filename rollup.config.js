bbe09b5c69e5c1ae80bd6738ccd8fb7d985c818c
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-05-03 11:03:40 +0300

    move babel config to rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,11 @@
 import babel from 'rollup-plugin-babel';
 
+const babelOptions = {
+  babelrc: false,
+  presets: ['es2017', 'stage-2'],
+  plugins: ['external-helpers']
+}
+
 export default {
-  plugins: [babel()]
+  plugins: [babel(babelOptions)]
 };
