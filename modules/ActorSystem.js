a50e9b5321fccc8296456875cc67cbfd34620286
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-05-07 12:34:40 +0300

    actorOf returns ActorRef

--- modules/ActorSystem.js
+++ modules/ActorSystem.js
@@ -1,26 +1,28 @@
 import Mailbox from './Mailbox.js';
+import ActorRef from './ActorRef.js';
 
 export default class ActorSystem {
   constructor(dispatcher) {
     this.dispatcher = dispatcher;
     this.actors = new Map();
+    this.refs = new Map();
   }
 
   actorOf(Actor, name = Actor.name) {
-    if (this.actors.has(name)) {
-      return this.actors.get(name);
-    }
+    if (this.refs.has(name)) return this.refs.get(name);
 
     const instance = new Actor(this);
+    const ref = new ActorRef();
     const mailbox = this.dispatcher.mailboxOf(Mailbox);
 
     this.actors.set(name, instance);
+    this.refs.set(name, ref);
 
     this.spawn(async function process(system, instance, mailbox) {
       for await (const message of mailbox) instance.receive(message);
     }, instance, mailbox);
 
-    return instance;
+    return ref;
   }
 
   spawn(coroutine, ...args) {
