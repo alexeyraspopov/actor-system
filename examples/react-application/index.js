414e16b45445f7019a819dadb8c8769b42d86ace
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-05-09 19:53:40 +0300

    spawn a dumb message each second

--- examples/react-application/index.js
+++ examples/react-application/index.js
@@ -4,6 +4,7 @@ import Coroutine from 'react-coroutine';
 import { ActorSystem, MessageDispatcher, ExecutionContext, AnimationFrameExecutor, Message } from 'actor-system';
 import Application from './modules/Application.react';
 import Storage from './modules/Storage';
+import { SomeMessage } from './modules/Messages';
 
 const executor = new AnimationFrameExecutor();
 const context = new ExecutionContext(executor);
@@ -11,7 +12,15 @@ const dispatcher = new MessageDispatcher(context);
 const system = new ActorSystem(dispatcher);
 const storage = new Storage();
 
+async function Main(system) {
+  setInterval(() => {
+    system.dispatcher.dispatch(new SomeMessage(performance.now()));
+  }, 1000);
+}
+
 ReactDOM.render((
   <Application system={system} storage={storage}>
   </Application>
 ), document.querySelector('main'));
+
+system.spawn(Main);
---
+++ examples/react-application/modules/Messages.js
@@ -0,0 +1,3 @@
+import { Message } from 'actor-system';
+
+export class SomeMessage extends Message {}
