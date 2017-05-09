da3672843b59a33209ee7c914b40bb23396cb141
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-05-09 20:05:34 +0300

    reorder exports

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-export { default as ExecutionContext } from './modules/ExecutionContext';
 export { default as AnimationFrameExecutor } from './modules/AnimationFrameExecutor';
-export { default as MessageDispatcher } from './modules/MessageDispatcher';
+export { default as ExecutionContext } from './modules/ExecutionContext';
 export { default as Mailbox } from './modules/Mailbox';
 export { default as Message } from './modules/Message';
-export { default as ActorSystem } from './modules/ActorSystem';
+export { default as MessageDispatcher } from './modules/MessageDispatcher';
 export { default as Actor } from './modules/Actor';
+export { default as ActorRef } from './modules/ActorRef';
+export { default as ActorSystem } from './modules/ActorSystem';
