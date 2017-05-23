3a27f21a0f2d06432e18c334b1b3be7df67567bb
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-05-23 16:28:11 -0400

    compile to es5

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import babel from 'rollup-plugin-babel';
 
 const babelOptions = {
   babelrc: false,
-  presets: ['es2017', 'stage-2'],
+  presets: [['es2015', { modules: false, loose: true }], 'stage-2'],
   plugins: ['external-helpers']
 }
 
