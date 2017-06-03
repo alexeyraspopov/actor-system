0f6501e2e140f25dc0d13e086bdd81dfa8c4aa6a
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-06-03 18:00:04 -0400

    use bootstrap system for react-application example

--- examples/react-application/index.js
+++ examples/react-application/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ActorSystem, MessageDispatcher, ExecutionContext, AnimationFrameExecutor } from 'actor-system';
+import { ActorSystem } from 'actor-system';
 import Application from './modules/Application.react';
 import Wrapper from './modules/Wrapper.react';
 import Storage from './modules/Storage';
 import Main from './modules/Main.actor';
 
-const executor = new AnimationFrameExecutor();
-const context = new ExecutionContext(executor);
-const dispatcher = new MessageDispatcher(context);
-const system = new ActorSystem(dispatcher);
+const system = ActorSystem.fromDefaults();
 const storage = new Storage();
 
 ReactDOM.render((
